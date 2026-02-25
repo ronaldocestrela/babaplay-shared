22cf99eb7a63a7ef00bd5b4fa39d70b8b1696790
Ronaldo Estrela <ronaldoestrela@yahoo.com.br>
2026-02-24 23:24:00 -0300

    Add note explaining default roles in RoleConstants

--- BabaPlayShared.Library/Constants/RoleConstants.cs
+++ BabaPlayShared.Library/Constants/RoleConstants.cs
@@ -4,6 +4,7 @@ namespace BabaPlayShared.Library.Constants;
 
 public static class RoleConstants
 {
+    // NOTE: These are the default roles that are created when the application is first run. They should not be changed or removed, as they are used for seeding the database and for role-based authorization throughout the application.
     public const string Admin = nameof(Admin);
     public const string Basic = nameof(Basic);
 
