6de0799a77431bb0ba974e1ae6bfb01829d6d0d4
Ronaldo Estrela <ronaldoestrela@yahoo.com.br>
2026-02-26 10:25:49 -0300

    Make properties in RefreshTokenRequest nullable

--- BabaPlayShared.Library/Models/Requests/Token/RefreshTokenRequest.cs
+++ BabaPlayShared.Library/Models/Requests/Token/RefreshTokenRequest.cs
@@ -2,7 +2,7 @@ namespace BabaPlayShared.Library.Models.Requests.Token;
 
 public class RefreshTokenRequest
 {
-    public required string CurrentJwt { get; set; }
-    public required string CurrentRefreshToken { get; set; }
-    public required DateTime RefreshTokenExpiryDate { get; set; }
+    public string? CurrentJwt { get; set; }
+    public string? CurrentRefreshToken { get; set; }
+    public DateTime? RefreshTokenExpiryDate { get; set; }
 }
